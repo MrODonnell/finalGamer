f4301437a74c5f5a8b20536576d0c13884510cd8
Beth Newton <32069726+BeNewt@users.noreply.github.com>
2018-05-01 11:34:06 -0400

    Ideas for my final game

---
+++ ideas for final game.py
@@ -0,0 +1,83 @@
+# A text-based adventure game 
+
+# The game will be object oriented  
+
+# A game about wrestlers 
+
+# Wrestlers will be my original characters 
+
+# I will make biographies for my characters 
+
+# I will make my own wrestling moves 
+
+# I will come up with my own championships, payperviews, and match stipulations 
+
+# There will be multiple classes 
+
+#  There will be a class for wrestlers 
+
+# There will be a class for payperviews 
+
+# There will be a class for championships 
+
+# There will be a class for weapons 
+
+# There will be a class for match stipulations 
+
+# There will be a class for moves 
+
+# Winning matches increases health 
+
+# Losing matches decreases health 
+
+# Win matches to win championships 
+
+# Matches will be held at payperviews 
+
+# Matches will be fought with weapons 
+
+# Matches will have stipulations 
+
+# Matches will be fought to win championship 
+
+# Weapons and moves will have value 
+
+# Weapons and moves will subtract health points from opponents 
+
+# The player will be shown the names and weights of the wrestlers 
+
+# The player will not be able to select the same wrestler as the champion and the challenger 
+
+# Once the player selects a wrestler their name will be taken off the list 
+
+# The name of the objects must be valid 
+
+# Player will select their character  
+
+# Player will pick their opponent 
+
+# The player's character will be the champion and their opponent will be the challenger 
+
+# The player will pick the general moves of the champion and the challenger 
+
+# The player will pick the weapons for the champion and the challenger 
+
+# The player will pick the match stipulations for the match 
+
+# The player will pick the payperview where the match will occur 
+
+# The player will pick the championship that they are fighting over 
+
+# Player will not know the value of moves or weapons 
+
+# After the player selects everything the match will begin 
+
+# The health points will be subtracted from the characters 
+
+# The winner will be decided based on the amount of health points left 
+
+# The winner will receive and/or retain the championship 
+
+# Losing a match will decrease overall health points 
+
+# Winning a match will increase overall health points 
